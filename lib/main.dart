436dcac1d321270983f564eda815c068086000d1
Demetre <demetrepanjakidze1@gmail.com>
2023-05-22 12:23:54 +0400

    some progress

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,14 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const CalculatorApp());
+}
+
+class CalculatorApp extends StatelessWidget {
+  const CalculatorApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Calculator app');
+  }
+}
